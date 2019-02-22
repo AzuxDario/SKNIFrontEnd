41ca2dc44168c465e8fbeb84862ac5760bb61db7
Paweł Dziedzic <blavikenn@gmail.com>
2019-02-22 21:16:23 +0100

    Add Register, Login and Logout buttons for User Panel

--- src/components/UserPanel.vue
+++ src/components/UserPanel.vue
@@ -1,17 +1,47 @@
 <template>
-    <div>
-        USER
-    </div>
+  <v-container>
+    <img src="../assets/czarnakaczka.jpg">
+
+    <v-container v-if="isLogged">
+      <v-card-actions>
+        <v-btn primary large block @click="logout()">Logout</v-btn>
+      </v-card-actions>
+    </v-container>
+    <v-container v-else>
+      <v-card-actions>
+        <v-btn primary large block @click="goToLogin()">Login</v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <v-btn primary large block @click="goToRegister()">Register</v-btn>
+      </v-card-actions>
+    </v-container>
+  </v-container>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
+<script lang='ts'>
+import Vue from "vue";
+import Component from "vue-class-component";
+import { isLoggedIn, logout } from "@/helpers/auth.ts";
 
 @Component
 export default class UserPanel extends Vue {
-    public name = 'UserPanel';
+  private isLogged = false;
 
+  private mounted() {
+    isLoggedIn().then((r) => (this.isLogged = r));
+  }
+
+  private goToLogin() {
+    this.$router.push("/login");
+  }
+
+  private goToRegister() {
+    this.$router.push("/register");
+  }
+
+  private logout() {
+    logout();
+    this.isLogged = false;
+  }
 }
-
 </script>
